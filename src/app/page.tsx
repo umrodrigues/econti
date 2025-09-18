0bf6067630f2ba3ee38ce7831cb579d8331e790f
luarodriguess <lua.rodrigues@lydia.com.br>
2025-09-18 17:33:52 -0300

    feat: create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import Banner from '../../components/Banner';
 import MiniBanners from '../../components/MiniBanners';
 import Portfolio from '../../components/Portfolio';
 import LeadershipSection from '../../components/LeadershipSection';
+import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-      <Banner />
-      <MiniBanners />
-      <LeadershipSection />
-      <Portfolio />
-    </main>
+      <main>
+        <Banner />
+        <MiniBanners />
+        <LeadershipSection />
+        <Portfolio />
+      </main>
+      <Footer />
+    </>
   );
 }
